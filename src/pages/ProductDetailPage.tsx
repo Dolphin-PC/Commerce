a49c56df4757586d4ecca2d9f1600fb63b4c30b9
dolphin-pc <pcx474@gmail.com>
2024-08-23 09:36:46 +0900

    제품 상세페이지 [목록으로] 버튼추가

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -5,6 +5,7 @@ import { useAuthStore } from "@/features/auth/auth.store";
 import { H2, H4 } from "@/shared/components/atoms/Typography";
 import Error from "@/shared/components/molecules/Error";
 import Loading from "@/shared/components/molecules/Loading";
+import Column from "@/shared/components/styles/Column";
 import Row from "@/shared/components/styles/Row";
 import CenterLayout from "@/shared/components/templates/CenterLayout";
 import DashBoardLayout from "@/shared/components/templates/DashBoardLayout";
@@ -12,7 +13,7 @@ import { Button } from "@/shared/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/shared/components/ui/card";
 import { ROUTES } from "@/shared/consts/route.const";
 import { useLayoutEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
@@ -45,53 +46,58 @@ const ProductDetailPage = () => {
 
   return (
     <DashBoardLayout>
-      <Card>
-        <CardHeader>
-          <Row className="justify-between">
-            <H2>상품 정보</H2>
+      <Column className="gap-3">
+        <Button asChild className="w-[100px]">
+          <Link to={ROUTES.DASHBOARD__PRODUCTS}>상품 목록</Link>
+        </Button>
+        <Card>
+          <CardHeader>
+            <Row className="justify-between">
+              <H2>상품 정보</H2>
 
-            <Row className="gap-5">
-              <Button>삭제</Button>
-              <Button>수정</Button>
+              <Row className="gap-5">
+                <Button>삭제</Button>
+                <Button>수정</Button>
+              </Row>
             </Row>
-          </Row>
-        </CardHeader>
+          </CardHeader>
 
-        <CardContent>
           <CardContent>
-            <H4>상품 카테고리</H4>
-            <p>{category.data?.categoryName}</p>
+            <CardContent>
+              <H4>상품 카테고리</H4>
+              <p>{category.data?.categoryName}</p>
+            </CardContent>
+            <CardContent>
+              <H4>상품 명</H4>
+              <p>{product.data?.name}</p>
+            </CardContent>
+            <CardContent>
+              <H4>상품 가격</H4>
+              <p>{product.data?.price.toLocaleString("ko-KR")} 원</p>
+            </CardContent>
+            <CardContent>
+              <H4>상품 수량</H4>
+              <p>{product.data?.quantity.toLocaleString("ko-KR")} 개</p>
+            </CardContent>
+            <CardContent>
+              <H4>상품 이미지</H4>
+              <Row className="gap-5 flex-wrap">
+                {productImage.data?.length === 0 && (
+                  <p>등록된 이미지가 없습니다.</p>
+                )}
+                {productImage.data?.map((image) => (
+                  <img
+                    key={image.id}
+                    src={image.imgUrl}
+                    alt={product.data?.name}
+                    style={{ width: "100px" }}
+                  />
+                ))}
+              </Row>
+            </CardContent>
           </CardContent>
-          <CardContent>
-            <H4>상품 명</H4>
-            <p>{product.data?.name}</p>
-          </CardContent>
-          <CardContent>
-            <H4>상품 가격</H4>
-            <p>{product.data?.price.toLocaleString("ko-KR")} 원</p>
-          </CardContent>
-          <CardContent>
-            <H4>상품 수량</H4>
-            <p>{product.data?.quantity.toLocaleString("ko-KR")} 개</p>
-          </CardContent>
-          <CardContent>
-            <H4>상품 이미지</H4>
-            <Row className="gap-5 flex-wrap">
-              {productImage.data?.length === 0 && (
-                <p>등록된 이미지가 없습니다.</p>
-              )}
-              {productImage.data?.map((image) => (
-                <img
-                  key={image.id}
-                  src={image.imgUrl}
-                  alt={product.data?.name}
-                  style={{ width: "100px" }}
-                />
-              ))}
-            </Row>
-          </CardContent>
-        </CardContent>
-      </Card>
+        </Card>
+      </Column>
     </DashBoardLayout>
   );
 };
