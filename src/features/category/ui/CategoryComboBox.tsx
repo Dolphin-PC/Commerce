34aac55b4f4f083d60c00c824246fe9e729f7b41
dolphin-pc <pcx474@gmail.com>
2024-08-26 16:53:43 +0900

    카테고리 콤보박스 컴포넌트 재사용을 위한 수정

--- src/features/category/ui/CategoryComboBox.tsx
+++ src/features/category/ui/CategoryComboBox.tsx
@@ -1,38 +1,44 @@
+import Loading from "@/shared/components/molecules/Loading";
 import { Button } from "@/shared/components/ui/button";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/shared/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/shared/components/ui/popover";
-
 import { cn } from "@/shared/lib/shadcn-util";
 import { Check, ChevronsUpDown } from "lucide-react";
-
-import { ProductFormDataType } from "@/features/product/model/product.zod";
-import { useEffect, useState } from "react";
-import { ControllerRenderProps, UseFormReturn } from "react-hook-form";
+import { useState } from "react";
+import { useCategoryListQuery } from "../api/get_list-category";
 import { Category } from "../model/type";
-import { getCategoryList } from "../api/get_list-category";
 
 interface Props {
-  field: ControllerRenderProps<ProductFormDataType>;
-  form: UseFormReturn<ProductFormDataType>;
+  // field: ControllerRenderProps<ProductFormDataType>;
+  // form: UseFormReturn<ProductFormDataType>;
   defaultCategoryId?: number;
+  onSelect: (id: number) => void;
 }
 
-const CategoryComboBox = ({ field, form }: Props) => {
+const CategoryComboBox = ({ onSelect }: Props) => {
   const [open, setOpen] = useState(false);
 
-  const [categoryList, setCategoryList] = useState<Category[]>([]);
+  const { data: categoryList, isLoading } = useCategoryListQuery({});
+  const [selected, setSelected] = useState<Category | null>(null);
 
-  useEffect(() => {
-    getCategoryList({}).then((res) => {
-      setCategoryList(res);
-    });
-  }, []);
+  if (isLoading) return <Loading />;
+  if (!categoryList) return <p>등록된 카테고리가 없어요.</p>;
+
+  const handleSelect = (categoryName: string) => {
+    const category = categoryList.find((c) => c.categoryName === categoryName);
+    if (!category) throw new Error(`Category not found: ${categoryName}`);
+
+    setSelected(category);
+    onSelect(category.id);
+    setOpen(false);
+  };
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
-          {field.value ? categoryList.find((c) => c.id === field.value)?.categoryName : "카테고리 선택"}
+          {selected ? selected.categoryName : "카테고리 선택"}
+          {/* {field.value ? categoryList.find((c) => c.id === field.value)?.categoryName : "카테고리 선택"} */}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
@@ -43,16 +49,8 @@ const CategoryComboBox = ({ field, form }: Props) => {
             <CommandEmpty>검색된 카테고리가 없어요.</CommandEmpty>
             <CommandGroup>
               {categoryList.map((c) => (
-                <CommandItem
-                  key={c.id}
-                  value={String(c.id)}
-                  onSelect={(id) => {
-                    form.setValue("categoryId", Number(id));
-                    form.trigger("categoryId");
-                    setOpen(false);
-                  }}
-                >
-                  <Check className={cn("mr-2 h-4 w-4", field.value === c.categoryName ? "opacity-100" : "opacity-0")} />
+                <CommandItem key={c.id} value={c.categoryName} onSelect={handleSelect}>
+                  <Check className={cn("mr-2 h-4 w-4", selected?.categoryName === c.categoryName ? "opacity-100" : "opacity-0")} />
                   {c.categoryName}
                 </CommandItem>
               ))}
