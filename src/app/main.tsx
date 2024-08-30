31e0a732bb9bd0af798e3e00150e98d53c894b65
Chanyoung Park <43159931+Dolphin-PC@users.noreply.github.com>
2024-08-30 12:37:35 +0900

    #24 react의 error boundary를 이용하여 효과적으로 에러 처리하기 (#47)
    * Error Boundary 수정
    
    * main.tsx에서의 try-catch로 React앱 구성 오류시, catch

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+try {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} catch (error) {
+  document.getElementById("root")!.innerHTML = `
+    <div style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      height: 100vh;
+      text-align: center;
+    ">
+      <h1 style="font-size: 50px">React App 오류</h1>
+      
+    </div>
+  `;
+  console.error(error);
+}
--- src/app/providers/RootProvider.tsx
+++ src/app/providers/RootProvider.tsx
@@ -1,7 +1,5 @@
-import ErrorFallback from "@/shared/components/molecules/ErrorFallback";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ErrorBoundary } from "react-error-boundary";
 import { Fragment } from "react/jsx-runtime";
 
 const queryClient = new QueryClient();
@@ -12,12 +10,10 @@ interface Props {
 export const RootProvider = ({ children }: Props) => {
   return (
     <Fragment>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={true} />
-          {children}
-        </QueryClientProvider>
-      </ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        {children}
+      </QueryClientProvider>
     </Fragment>
   );
 };
