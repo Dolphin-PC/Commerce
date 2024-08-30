711406b51c294fa7c08911a4489504e31a9e1547
dolphin-pc <pcx474@gmail.com>
2024-08-30 15:35:23 +0900

    react helmet 적용

--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -1,9 +1,11 @@
 import { H2 } from "@/shared/components/atoms/Typography";
 import DashBoardLayout from "@/shared/components/templates/DashBoardLayout";
+import { DashBoardPageHelmet } from "./Helmets";
 
 const DashBoardPage = () => {
   return (
     <DashBoardLayout>
+      <DashBoardPageHelmet />
       <H2>대시보드</H2>
     </DashBoardLayout>
   );
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
 import MainLayout from "@/widgets/layout/MainLayout";
 import CategoryProductList from "./ui/CategoryProductList";
+import { HomePageHelmet } from "../Helmets";
 
 const HomePage = () => {
   return (
     <MainLayout>
+      <HomePageHelmet />
       <CategoryProductList />
     </MainLayout>
   );
