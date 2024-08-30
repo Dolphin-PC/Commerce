068f68d845c218698d61bf44dae27c435baec81f
dolphin-pc <pcx474@gmail.com>
2024-08-30 16:12:53 +0900

    rollup-plugin-visualizer 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr"
+import { defineConfig, type PluginOption } from "vite";
 import path from "path";
+
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    visualizer({
+      filename: "./dist/stats.html",
+      open: true,
+      brotliSize: true,
+    }) as PluginOption,
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
