ef208d9638cb3af190e753a2b70f4ea23ee7c8b0
dolphin-pc <pcx474@gmail.com>
2024-08-30 11:34:47 +0900

    Error Boundary 수정

--- src/app/routers/Router.tsx
+++ src/app/routers/Router.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootComponent from "../Common/RootComponent";
 import { PrivateRoute } from "./Private.route";
 import { SellerRoute } from "./Seller.route";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "@/shared/components/molecules/ErrorFallback";
 
 const HomePage = lazy(() => import("@/pages/HomePage/index"));
 const ProductPage = lazy(() => import("@/pages/ProductPage/index"));
@@ -26,30 +28,32 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <RootComponent />
-      <Suspense fallback={<CenterLoading />}>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path={ROUTES.SIGNIN} element={<SignInPage />} />
-          <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
-          <Route path={ROUTES.SIGNUP__OAUTH} element={<SignupOAuthPage />} />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<CenterLoading />}>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />} />
+            <Route path={ROUTES.SIGNIN} element={<SignInPage />} />
+            <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+            <Route path={ROUTES.SIGNUP__OAUTH} element={<SignupOAuthPage />} />
 
-          <Route path={ROUTES.PRODUCTS} element={<ProductPage />} />
-          <Route path={ROUTES.PRODUCTS_ID} element={<ProductDetailPage />} />
+            <Route path={ROUTES.PRODUCTS} element={<ProductPage />} />
+            <Route path={ROUTES.PRODUCTS_ID} element={<ProductDetailPage />} />
 
-          <Route element={<PrivateRoute />}>
-            <Route path={ROUTES.MY} element={<Button>my</Button>} />
+            <Route element={<PrivateRoute />}>
+              <Route path={ROUTES.MY} element={<Button>my</Button>} />
 
-            {/* 판매자 전용 */}
-            <Route element={<SellerRoute />}>
-              <Route path={ROUTES.DASHBOARD} element={<DashBoardPage />} />
-              <Route path={ROUTES.DASHBOARD__PRODUCTS} element={<DashboardProductPage />} />
-              <Route path={ROUTES.DASHBOARD__PRODUCTS__NEW} element={<ProductNewPage />} />
-              <Route path={ROUTES.DASHBOARD__PRODUCTS_ID} element={<DashboardProductDetailPage />} />
-              <Route path={ROUTES.DASHBOARD__PRODUCTS__EDIT_ID} element={<ProductEditPage />} />
+              {/* 판매자 전용 */}
+              <Route element={<SellerRoute />}>
+                <Route path={ROUTES.DASHBOARD} element={<DashBoardPage />} />
+                <Route path={ROUTES.DASHBOARD__PRODUCTS} element={<DashboardProductPage />} />
+                <Route path={ROUTES.DASHBOARD__PRODUCTS__NEW} element={<ProductNewPage />} />
+                <Route path={ROUTES.DASHBOARD__PRODUCTS_ID} element={<DashboardProductDetailPage />} />
+                <Route path={ROUTES.DASHBOARD__PRODUCTS__EDIT_ID} element={<ProductEditPage />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </Suspense>
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
--- src/shared/components/molecules/ErrorFallback.tsx
+++ src/shared/components/molecules/ErrorFallback.tsx
@@ -1,15 +1,31 @@
+import { Link } from "react-router-dom";
+import Row from "../atoms/Row";
+import { Muted } from "../atoms/Typography";
+import { Button } from "../ui/button";
+import { CenterError } from "./Error";
+import { ROUTES } from "@/shared/consts/route.const";
+
 interface Props {
   error: Error;
   resetErrorBoundary: () => void;
 }
 
 const ErrorFallback = ({ error, resetErrorBoundary }: Props) => {
+  console.error(error);
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+    <CenterError>
+      <div className="w-1/2 p-4 m-2 bg-slate-100 rounded-md">
+        <Muted>{error.message}</Muted>
+      </div>
+      <Row className="gap-3">
+        <Button variant="destructive" onClick={resetErrorBoundary}>
+          재시도
+        </Button>
+        <Button variant="default" asChild onClick={resetErrorBoundary}>
+          <Link to={ROUTES.HOME}>홈으로</Link>
+        </Button>
+      </Row>
+    </CenterError>
   );
 };
 
