20b213c8d5fad62c0e691355a60ecd837ad00e51
dolphin-pc <pcx474@gmail.com>
2024-09-01 10:06:15 +0900

    vitest 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-import { defineConfig, InlineConfig, UserConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
-interface VitestConfigExport extends UserConfig {
-  test: InlineConfig;
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
-  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-} as VitestConfigExport);
+});
