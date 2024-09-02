f0d7bd97c22a97c5d9853d2f832462e9576759f4
dolphin-pc <pcx474@gmail.com>
2024-09-02 18:57:00 +0900

    Supabase, Auth session missing 에러 해결

--- src/features/@auth/api/get-user.ts
+++ src/features/@auth/api/get-user.ts
@@ -10,7 +10,12 @@ interface Return {
 }
 
 export const getAuthUser = async (): Promise<Return> => {
+  const session = await supabase.auth.getSession();
+
+  if (!session.data.session) return { user: null };
+
   const { data, error } = await supabase.auth.getUser();
+  supabase.auth.reauthenticate;
 
   if (error) throw error;
 
