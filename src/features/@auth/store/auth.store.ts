aac9bdb431b1c1cbc9459ddb1b180ff8d7a73d53
dolphin-pc <pcx474@gmail.com>
2024-09-02 22:12:30 +0900

    userStore - getUser

--- src/features/@auth/store/auth.store.ts
+++ src/features/@auth/store/auth.store.ts
@@ -8,10 +8,16 @@ interface Props {
 
 interface Actions {
   setSignedIn: (user: User | null) => void;
+  getUser: () => User;
 }
 
-export const useAuthStore = create<Props & Actions>((set) => ({
+export const useAuthStore = create<Props & Actions>((set, get) => ({
   user: null,
   isLoading: true,
   setSignedIn: (user) => set({ user, isLoading: false }),
+  getUser: () => {
+    const user = get().user;
+    if (!user) throw new Error("User is not signed in");
+    return user;
+  },
 }));
