9ef8fc5075daac9bbcf1dba5319afb73ca3a92c6
dolphin-pc <pcx474@gmail.com>
2024-09-05 13:19:08 +0900

    MainLayout Props, className 추가

--- src/widgets/layout/MainLayout.tsx
+++ src/widgets/layout/MainLayout.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/shared/lib/shadcn-util";
 import Header from "./Header";
 
 interface Props {
   children: React.ReactNode;
+  className?: HTMLDivElement["className"];
+  mainClassName?: HTMLDivElement["className"];
 }
-const MainLayout = ({ children }: Props) => {
+const MainLayout = ({ children, className, mainClassName }: Props) => {
   return (
-    <div>
+    <div className={className}>
       <div className="sticky top-0 p-4 bg-white z-50" style={{ boxShadow: "0px 2px 2px -2px gray" }}>
         <Header />
       </div>
-      <div className="container mt-8">{children}</div>
+      <main className={cn("container mt-8", mainClassName)}>{children}</main>
     </div>
   );
 };
