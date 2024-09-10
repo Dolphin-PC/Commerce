1710a1416041598f6240d9fa1b886f2a60016ef6
dolphin-pc <pcx474@gmail.com>
2024-09-10 12:57:28 +0900

    custom commands로 테스트코드 모듈화

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,12 +1,13 @@
 beforeEach(() => {});
 
 it("로그인 성공", () => {
-  cy.visit("/sign-in");
+  // cy.visit("/sign-in");
 
-  cy.findByLabelText("Email").type(Cypress.env("userId"));
-  cy.findByLabelText("Password").type(Cypress.env("password"));
+  // cy.findByLabelText("Email").type(Cypress.env("userId"));
+  // cy.findByLabelText("Password").type(Cypress.env("password"));
 
-  cy.findByText("Sign In").click();
+  // cy.findByText("Sign In").click();
+  cy.login();
 
-  cy.url().should("eq", `${Cypress.env("baseUrl")}/`);
+  cy.assertUrl("/");
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,16 +2,50 @@
 
 import "@testing-library/cypress/add-commands";
 
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      assertUrl(url: string): Chainable<void>;
+      login(): Chainable<void>;
+    }
+  }
+}
+
+// custom commands https://docs.cypress.io/api/cypress-api/custom-commands
+// custom queries https://docs.cypress.io/api/cypress-api/custom-queries
+
+Cypress.Commands.add("assertUrl", (url) => {
+  cy.url().should("eq", `${Cypress.env("baseUrl")}${url}`);
+});
+
+Cypress.Commands.add("login", () => {
+  const userId = Cypress.env("userId");
+  const password = Cypress.env("password");
+
+  cy.session(userId, () => {
+    cy.visit("/sign-in");
+
+    cy.findByLabelText("Email").type(userId);
+    cy.findByLabelText("Password").type(password);
+    cy.findByText("Sign In").click();
+
+    cy.location("pathname").should("eq", "/");
+  });
+
+  cy.visit("/");
+});
+
+// 커스텀 쿼리 예제
+// Cypress.Commands.addQuery('get', () => {
+//     const getFn = cy.now('get', '[data-testid="cart-icon"]');
+
+// inner function 형태로 반환해야 함
+//     return subject => {
+//         const btn = getFn(subject);
+
+//         return btn;
+//     }
+// })
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
@@ -28,13 +62,3 @@ import "@testing-library/cypress/add-commands";
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
