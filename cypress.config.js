2eb48839d1fd8b7cd8071b82e5fc7f11df8832fa
dolphin-pc <pcx474@gmail.com>
2024-09-10 17:24:40 +0900

    update cypress.config

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-var baseUrl = "http://localhost:5173";
-export default defineConfig({
-    e2e: {
-        baseUrl: baseUrl,
-        viewportWidth: 1440,
-        viewportHeight: 1000,
-    },
-});
