b109a84cc31fdda92c769fcdb4fc4cbed84fbc77
dolphin-pc <pcx474@gmail.com>
2024-09-10 16:24:01 +0900

    로그인 분리 (판매자, 일반)

--- cypress/e2e/SignInPage.cy.ts
+++ cypress/e2e/SignInPage.cy.ts
@@ -1,7 +1,7 @@
 beforeEach(() => {});
 
 it("로그인 성공", () => {
-  cy.login();
+  cy.login(false);
 
   cy.assertUrl("/");
   cy.findByTestId("user-icon-button").should("exist");
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,7 +11,7 @@ declare global {
   namespace Cypress {
     interface Chainable {
       assertUrl(url: string): Chainable<void>;
-      login(): Chainable<void>;
+      login(isSeller: boolean): Chainable<void>;
       interceptor_supabase_auth(method: Method, response?: RouteHandler): Chainable<() => {}>;
       interceptor_supabase_db(method: Method, table: Table, response?: RouteHandler): Chainable<() => {}>;
     }
@@ -25,15 +25,21 @@ Cypress.Commands.add("assertUrl", (url) => {
   cy.url().should("eq", `${Cypress.env("baseUrl")}${url}`);
 });
 
-Cypress.Commands.add("login", () => {
-  const userId = Cypress.env("userId");
-  const password = Cypress.env("password");
+Cypress.Commands.add("login", (isSeller) => {
+  let id, pw;
+  if (isSeller) {
+    id = Cypress.env("sellerId");
+    pw = Cypress.env("sellerPw");
+  } else {
+    id = Cypress.env("userId");
+    pw = Cypress.env("userPw");
+  }
 
-  cy.session(userId, () => {
+  cy.session(id, () => {
     cy.visit("/sign-in");
 
-    cy.findByLabelText("Email").type(userId);
-    cy.findByLabelText("Password").type(password);
+    cy.findByLabelText("Email").type(id);
+    cy.findByLabelText("Password").type(pw);
     cy.findByText("Sign In").click();
 
     cy.location("pathname").should("eq", "/");
