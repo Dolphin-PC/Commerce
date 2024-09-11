fd5e9ba8a46e98e774dbc0716d6f653646a4c77d
dolphin-pc <pcx474@gmail.com>
2024-09-11 17:34:34 +0900

    최신상품 순 UI 추가

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,41 @@
 import MainLayout from "@/widgets/MainLayout";
 import CategoryProductList from "./ui/CategoryProductList";
 import { HomePageHelmet } from "../Helmets";
-import { Fragment } from "react/jsx-runtime";
+import { Card, CardContent, CardHeader, CardTitle } from "@/shared/components/ui/card";
+import Column from "@/shared/components/atoms/Column";
+import { useProductListCategoryQuery } from "@/features/product/api/get_list-product_category";
+import ProductCard from "@/features/product/ui/ProductCard";
+import { Link } from "react-router-dom";
+import { ROUTES } from "@/shared/consts/route.const";
+import { Button } from "@/shared/components/ui/button";
+import Grid from "@/shared/components/atoms/Grid";
 
 const _HomePage = () => {
+  const { data: newProducts } = useProductListCategoryQuery({ order: { column: "createdAt", ascending: false }, pageSize: 4 });
   return (
-    <Fragment>
+    <Column className="gap-10">
+      <Card>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <CardTitle>최신 상품</CardTitle>
+          <Button variant="link" asChild>
+            <Link to={ROUTES.PRODUCTS}>더보기</Link>
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Grid className="grid-cols-4 gap-3">
+            {newProducts?.data.map((product) => {
+              return (
+                <Link key={product.id} to={ROUTES.PRODUCTS_ID_(product.id)}>
+                  <ProductCard product={product} category={product.category} />
+                </Link>
+              );
+            })}
+          </Grid>
+        </CardContent>
+      </Card>
       <CategoryProductList />
-    </Fragment>
+    </Column>
   );
 };
 
