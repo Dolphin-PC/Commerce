efeab829fc2aecbf37cd3cf76771bd5c7758f907
dolphin-pc <pcx474@gmail.com>
2024-09-12 22:37:16 +0900

    홈 > 카테고리 더보기 핉터링

--- src/pages/HomePage/ui/CategoryProductList.tsx
+++ src/pages/HomePage/ui/CategoryProductList.tsx
@@ -7,6 +7,7 @@ import { T } from "@/shared/components/atoms/Typography";
 import { Button } from "@/shared/components/ui/button";
 import { TabsContent } from "@/shared/components/ui/tabs";
 import { ROUTES } from "@/shared/consts/route.const";
+import { useSearchStore } from "@/widgets/ProductSearchDrawer/store/useSearchStore";
 import { Link, useNavigate } from "react-router-dom";
 
 interface Props {
@@ -17,21 +18,22 @@ const CategoryProductList = ({ category }: Props) => {
   const navigate = useNavigate();
   const { data: categoryProducts } = useProductListCategoryQuery({ categoryId: category.id, pageSize: 4 });
 
-  /** 상품 목록 페이지 이동 */
-  const moveToProductPage = (categoryId: number) => {
-    const query = new URLSearchParams();
-    query.set("categoryId", String(categoryId));
+  const setCategoryIds = useSearchStore((state) => state.setCategoryIds);
 
-    navigate(`${ROUTES.PRODUCTS}?${query.toString()}`);
+  /** 상품 목록 페이지 이동 */
+  const moveToProductPage = () => {
+    setCategoryIds([category.id]);
+
+    navigate(ROUTES.PRODUCTS);
   };
 
   return (
     <TabsContent key={category.id} value={String(category.id)} className="sticky left-0">
       <Row className="justify-between">
-        <Button variant="link" onClick={() => moveToProductPage(category.id)}>
+        <Button variant="link" onClick={moveToProductPage}>
           <T.Lead>{category.categoryName}</T.Lead>
         </Button>
-        <Button variant="link" onClick={() => moveToProductPage(category.id)}>
+        <Button variant="link" onClick={moveToProductPage}>
           더보기
         </Button>
       </Row>
