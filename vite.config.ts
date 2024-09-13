a983c7a54e6bb763d3106df3b7da565838dc7e3e
dolphin-pc <pcx474@gmail.com>
2024-09-13 12:45:00 +0900

    removeConsole

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), removeConsole()],
 
   resolve: {
     alias: {
