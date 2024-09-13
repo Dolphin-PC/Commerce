84f47747ac88517fa7151a4270a565a52e1b47a3
dolphin-pc <pcx474@gmail.com>
2024-09-13 09:21:46 +0900

    상품상세 > 추천상품 더보기

--- src/pages/ProductDetailPage/ui/RecommendProductList.tsx
+++ src/pages/ProductDetailPage/ui/RecommendProductList.tsx
@@ -5,10 +5,12 @@ import ProductCard from "@/features/product/ui/ProductCard";
 import Column from "@/shared/components/atoms/Column";
 import Grid from "@/shared/components/atoms/Grid";
 import Row from "@/shared/components/atoms/Row";
-import { H3 } from "@/shared/components/atoms/Typography";
+import { T } from "@/shared/components/atoms/Typography";
 import { Badge } from "@/shared/components/ui/badge";
+import { Button } from "@/shared/components/ui/button";
 import { ROUTES } from "@/shared/consts/route.const";
-import { Link } from "react-router-dom";
+import { useSearchStore } from "@/widgets/ProductSearchDrawer/store/useSearchStore";
+import { Link, useNavigate } from "react-router-dom";
 
 interface Props {
   id: Product["id"];
@@ -19,14 +21,27 @@ interface Props {
  * @desc 해당 상품의 카테고리에 속하는 상품 목록
  */
 const RecommendProductList = ({ category, id }: Props) => {
+  const navigate = useNavigate();
   const { data } = useProductListCategorySuspenseQuery({ categoryId: category.id, pageNumber: 0, pageSize: 9 });
+  const setCategoryIds = useSearchStore((state) => state.setCategoryIds);
+
+  const moveToListPage = () => {
+    setCategoryIds([category.id]);
+    navigate(ROUTES.PRODUCTS);
+  };
+
   return (
     <Column className="gap-3">
-      <Row className="gap-3 items-center">
-        <Badge variant={"outline"} size={"large"}>
-          {category.categoryName}
-        </Badge>
-        <H3>다른 상품</H3>
+      <Row className="justify-between">
+        <Row className="gap-3 items-center">
+          <Badge variant={"outline"} size={"large"}>
+            {category.categoryName}
+          </Badge>
+          <T.H3>추천 상품</T.H3>
+        </Row>
+        <Button onClick={moveToListPage} variant="outline">
+          더보기
+        </Button>
       </Row>
 
       <Grid className="grid-cols-4 gap-4">
